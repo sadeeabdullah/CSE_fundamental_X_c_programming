e5fbc191e6ebc4c04ee08b3690b3667f6ca3a579
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-09 21:02:23 +0600

    adding first code of module 2

---
+++ module 2/arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum,subs,mul,div;
+    a = 5;
+    b = 7;
+    sum= a + b;
+    subs = a - b;
+    mul  = a * b;
+    div = a/b;
+    printf("%d \n",sum);
+    printf("%d \n",subs);
+    printf("%d \n",mul);
+    // printf("%d",div);
+    return 0;
+}
