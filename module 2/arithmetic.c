ab646e78e0da79f8be8ed472e0dd89e00807dba4
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-09 21:32:55 +0600

    adding notes

--- module 2/arithmetic.c
+++ module 2/arithmetic.c
@@ -11,5 +11,13 @@ int main(){
     printf("%d \n",subs);
     printf("%d \n",mul);
     // printf("%d",div);
+  
+    //we need to keep one variable in float to get the float value of any operation
+    
+    int y;
+    float x=5;
+    y= 2;
+    float division = x/y;
+    printf("%.2f",division);
     return 0;
 }
