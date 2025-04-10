7a40549862bf649e6347fe9c6e45a7395777a351
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-10 15:01:10 +0600

    adding more loop and varieties

---
+++ module 3/more_loop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    //printing the odd number from 1 - 100
+    for (int i = 1; i < 100; i+=2)
+    {
+        printf("%d\n",i);
+    }
+
+    //printing the multiples of 4
+    for (int i = 1; i <= 100; i++)
+    {
+        printf("%d\n",4*i);
+    }
+    
+    
+    return 0;
+}
