700954c3248d2a86a6bccff237030fea5338f51c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-15 19:24:06 +0600

    D-fixed password problem solution added

--- Module 6/D_Fixed_password.c
+++ Module 6/D_Fixed_password.c
@@ -2,6 +2,19 @@
 
 #include<stdio.h>
 int main(){
+    int password;
+    while (password != 1999)
+    {
+        scanf("%d",&password);
+        if (password == 1999)
+        {
+            printf("Correct\n");
+            break;
+        }else{
+            printf("Wrong\n");
+        }
+        
+    }
     
     return 0;
 }
