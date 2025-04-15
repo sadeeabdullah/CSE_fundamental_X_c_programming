56e1ed170d2d7d518a069088184420b78c5ea847
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-15 21:45:02 +0600

    solution added of digit problem

--- Module 6/Q_Digits.c
+++ Module 6/Q_Digits.c
@@ -1,21 +1,28 @@
-//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/Q
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/Q
 
-#include<stdio.h>
-int main(){
-    // int T,n;
-    // for (int tc = 1; tc <= T; tc++)
-    // {
-    int n;
-        scanf("%d",&n);
-        printf("%d",(n/10)%10);
-
-        // for (int i = 1; i < count; i++)
-        // {
-        //     /* code */
-        // }
-        
-
-    // }
+#include <stdio.h>
+int main()
+{
+    int T,n;
+    scanf("%d",&T);
+    for (int tc = 1; tc <= T; tc++)
+    {
+    int  digit;
+    scanf("%d", &n);
+    int updation = n;
     
+    
+   
+    do
+    {
+
+        digit = updation % 10;
+        updation = updation / 10;
+        printf("%d ", digit);
+    }while(updation != 0);
+    printf("\n");
+
+    }
+
     return 0;
 }
