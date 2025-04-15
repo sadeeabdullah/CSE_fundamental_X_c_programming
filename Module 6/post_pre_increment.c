b80e1f5c9ff00e67eeb2186b71f17448a2471568
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-15 17:28:56 +0600

    added more explaination of pre and post increment  in for loop.

--- Module 6/post_pre_increment.c
+++ Module 6/post_pre_increment.c
@@ -6,6 +6,18 @@ int main(){
     int y = x ++;
     //pre increment: will  increase the value before any other execution
     int z = ++f;
-    printf("%d %d",y,z);
+    printf("%d %d\n",f,z);
+    printf("%d %d \n",x,y);
+
+    //in loop it doesn't matter that we are using pre or post increment because we are not using this into the loop directly
+    for (int i = 1; i <= 5; i++)
+    {
+        printf("%d\n",i);
+    }
+    for (int i = 1; i <= 5; ++i)
+    {
+        printf("%d\n",i);
+    }
+    
     return 0;
 }
