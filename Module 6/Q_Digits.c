75411779e1fd6175a5bd7c439ed0a2554d48532b
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-15 20:32:57 +0600

    initializing the problem solution of digits

---
+++ Module 6/Q_Digits.c
@@ -0,0 +1,21 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/Q
+
+#include<stdio.h>
+int main(){
+    // int T,n;
+    // for (int tc = 1; tc <= T; tc++)
+    // {
+    int n;
+        scanf("%d",&n);
+        printf("%d",(n/10)%10);
+
+        // for (int i = 1; i < count; i++)
+        // {
+        //     /* code */
+        // }
+        
+
+    // }
+    
+    return 0;
+}
