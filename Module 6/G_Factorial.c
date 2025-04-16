c801dba0657d7b7a82dde826080fb1140a1aedb9
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-16 17:03:34 +0600

    solution added

--- Module 6/G_Factorial.c
+++ Module 6/G_Factorial.c
@@ -2,13 +2,29 @@
 
 #include<stdio.h>
 int main(){
-    // int tc;
-    // scanf("%d",tc);
-    // for (int i = 1; i <= tc; i++)
-    // {
+    int tc;
+    scanf("%d",&tc);
+    for (int i = 1; i <= tc; i++)
+    {
         
-    // }
+    int n;
+    scanf("%d",&n);
     
+    long long int factorial = n;
+    if (n == 0)
+    {
+        factorial = 1;
+    }
+
+    for (int j = n-1; j >= 1; j--)
+    {
+        factorial = factorial * j;
+
+    }
+    printf("%lld\n",factorial);
+    }
+    
+
     
     return 0;
 }
