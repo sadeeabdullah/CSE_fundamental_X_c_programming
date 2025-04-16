7502325eb7fa10c0e495cf35b22e5a3e76593898
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-16 16:50:56 +0600

    initializing factorial problem solution

---
+++ Module 6/G_Factorial.c
@@ -0,0 +1,14 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
+
+#include<stdio.h>
+int main(){
+    // int tc;
+    // scanf("%d",tc);
+    // for (int i = 1; i <= tc; i++)
+    // {
+        
+    // }
+    
+    
+    return 0;
+}
