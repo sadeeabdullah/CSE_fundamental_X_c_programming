7ef6c36916846cbd02c37f1dbf6dfc0b9902eb64
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-18 16:02:51 +0600

    second problem solution added

--- assisgnments/assignment 2/is_it_a_challenge.c
+++ assisgnments/assignment 2/is_it_a_challenge.c
@@ -3,6 +3,23 @@ int main(){
     int n;
     scanf("%d", &n);
     
+    if (n == 0)
+    {
+        printf("0");
+    }else if (n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%d ",i);
+        }
+        
+    }else{
+        for (int i = n; i <=0; i++)
+        {
+            printf("%d ",i);
+        }
+        
+    }
     
     return 0;
 }
