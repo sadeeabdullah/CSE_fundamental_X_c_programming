8058aab66967070dad671fa1c69e871701d7bb97
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-18 12:00:55 +0600

    adding A problem summation

---
+++ Module 7/array.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int arr[5];
+    arr[2]=15;
+    
+    return 0;
+}
---
+++ Practice Day 7.5/A_Summation.c
@@ -0,0 +1,18 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/A
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int arr_para;
+    long long int sum =0,n;
+    scanf("%d",&arr_para);
+    int arr[arr_para];
+    for (int i = 0; i < arr_para; i++)
+    {
+        scanf("%lld",&n);
+        sum+=n;
+    }
+    printf("%d",abs(sum));
+    
+    return 0;
+}
