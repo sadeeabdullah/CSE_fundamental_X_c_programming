d32a4a94a59030ee6f9b54a1b27f24c2415f0225
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-20 22:03:41 +0600

    G. palindrome array problem solution added

---
+++ practice 9/G_Palindrome_Array.c
@@ -0,0 +1,38 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/G
+
+#include<stdio.h>
+int main(){
+    int n, checker;
+    scanf("%d",&n);
+    int arr[n];
+
+    //taking input of the value in the array
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+    int i = 0, j = n-1;
+    while (i < j)
+    {
+        if (arr[i] == arr[j])
+        {
+            checker = 1;
+        }else{
+            checker = 0;
+        }
+        i++;
+        j--;
+        
+    }
+    
+    if (checker == 1)
+    {
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    
+    
+    return 0;
+}
