22e3ba7302f27b5b355b96bd675678e6b3a44b4f
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-21 21:04:13 +0600

    more concept about string

--- Module 10/string_input.c
+++ Module 10/string_input.c
@@ -3,8 +3,10 @@ int main(){
     char a[10];
     scanf("%s", &a);
     printf("%s",a);
+    printf("%c",a[6]);
     return 0;
 }
 
 //string put a null character at the after index of the last character of string that is how string get to know how much it need to print
 //\0 is null.
+// it set the null value only after the last index
