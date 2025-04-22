f4a64a9fdbcfbaf8dc092bb838a6eacb70899d80
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-04-22 18:05:10 +0600

    added E.count problem solution

--- Module 10/E_Count.c
+++ Module 10/E_Count.c
@@ -1,14 +1,17 @@
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/E
 #include<stdio.h>
 #include<string.h>
 int main(){
     char s[1000001];
-    scanf("%c",&s);
+    scanf("%s",&s);
     int length = strlen(s);
     int sum = 0;
     for (int i = 0; i < length ; i++)
     {
-        sum += s[i];
+        // printf("%d\n", s[i] - '0');
+        sum += s[i] - '0';
     }
+    printf("%d",sum);
     
     return 0;
 }
