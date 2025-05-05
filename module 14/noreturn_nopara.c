91cf49dbe4833da067676f88b65d58233e090145
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-05 19:13:29 +0600

    adding more built in function for math

--- module 14/noreturn_nopara.c
+++ module 14/noreturn_nopara.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void sum(int x, int y){ // no return so void
+void sum(){ // no return so void
  
-    int ans = x + y;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int ans = a + b;
     printf("%d", ans);
     return; // but we can use return to end the function
 }
 int main(){
-    int a, b;
-    scanf("%d %d", &a, &b);
-    sum(a, b);
+    sum();
     
     return 0;
 }
