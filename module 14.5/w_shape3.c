98c1f90a397fa99528c555e00f7e5ebf1042c8d1
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-05 20:48:31 +0600

    added T.shape2 problem solution

---
+++ module 14.5/w_shape3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int star = 1;
+    //for loop for printing the line
+    for (int i = 1; i <= n; i++)
+    {
+        //for loop for printing the star
+        for (int j = 1; j <= star;j++)
+        {
+            
+        }
+        
+    }
+    
+    return 0;
+}
