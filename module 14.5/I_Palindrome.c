d7083446cd0d29e7e5997eea3ba672c49b17c959
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-09 00:28:00 +0600

    palindrome problem solution added

--- module 14.5/I_Palindrome.c
+++ module 14.5/I_Palindrome.c
@@ -2,31 +2,25 @@
 int main(){
     int n, len = 0;
     scanf("%d", &n);
+
+    int original_digit = n;
+    int reversed = 0;
+
     //for getting the digits of number
     for (int i = 0; n > 0; i++)
     {
         int digit = n % 10;
-        printf("%d\n", digit);
-        len++;
+        
+        reversed = reversed * 10 + digit;
         n /= 10;
     }
-
-    // for (int i = 0; n > 0; i++) // for checking from the right side
-    // {
-    //     for (int i = 0; ; i++)
-    //     {
-    //         /* code */
-    //     }
-        
-    // }
-    printf("%d", len);
-
-    // while (n > 10)
-    // {
-    //     printf("%d\n", n / 10);
-    // }
-    
-    
+    printf("%d", reversed);
+    printf("\n");
+    if (original_digit == reversed)
+        printf("YES\n");
+    else
+        printf("NO\n");
+   
     
     return 0;
 }
