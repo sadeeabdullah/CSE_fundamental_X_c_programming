c2eecfbd34d61bd5adee1dcf0282a768f1b0200e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-17 18:23:50 +0600

    added lucky  division problem solution

--- Module 19/practice/J_Lucky_Division.c
+++ Module 19/practice/J_Lucky_Division.c
@@ -1,29 +1,39 @@
-#include<stdio.h>
-#include<stdbool.h>
-int main(){
-    int n;
-    int is_lucky = false;
-    scanf("%d", &n);
-    int original = n;
-    
-    while (n > 0)
+#include <stdio.h>
+#include <stdbool.h>
+//function to check if a number is lucky or not
+// all lucky number does have only 4 and 7 or  4 and 7 both
+
+bool is_lucky(int num){
+    while (num > 0)
     {
-        int digits = n % 10;
-        if (digits == 4 || digits == 7||n % 4 == 0 || n % 7 == 0)
+        int digit = num % 10;
+        if (digit != 4 && digit != 7)
         {
-            is_lucky = true;
+            return false;
         }
-        n /= 10;
+
+        return true;
         
     }
-   
     
-    if (is_lucky)
+}
+//almost lucky number is also divisible by some ( it can be more than 2)
+// we will find the lucky numbers from 1 to n and check if any lucky number can divide the given number then the number is lucky other wise not(and operator)
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
     {
-        printf("YES");
-    }else{
-        printf("NO");
+        if (is_lucky(i) && n % i == 0)
+        {
+            printf("YES");
+            return 0;
+        }
+        
     }
+    printf("NO");
     
     return 0;
 }
