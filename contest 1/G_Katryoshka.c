b67c2a4935ccae547c6e487da30821389ea9232a
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-24 17:32:48 +0600

    taken help fromthe ai

--- contest 1/G_Katryoshka.c
+++ contest 1/G_Katryoshka.c
@@ -1,40 +1,70 @@
 #include <stdio.h>
-int main()
-{
-    long long int n, m, k, katryoshka_count = 0;
+
+int main() {
+    long long int n, m, k;
     scanf("%lld %lld %lld", &n, &m, &k);
 
-    // if eyes or body is not available then no katryoshka is not possible to make
-    if (n == 0 || k == 0)
-    {
+    if (n == 0 || k == 0) {
         printf("0");
         return 0;
     }
 
-    // now checking if eyes and body is available if available the formation of katryoshka is possible according to the first formation
-    else if (n % 2 == 0 && k > 0 && m == 0)
-    {
-        // printf("hello from 2nd form");
-        while (n >= 2 && k >= 1)
-        {
-            katryoshka_count++; // katryoshka count will increase according to the first formation
-            n -= 2;             // eyes count will decrease by 2 because 2 eyes will be used to form a katryoshka
-            k--;                // 1 body count will decrase because one body will be taken for forming a katryoshka
+    long long int max_count = 0;
+    long long int original_n = n, original_m = m, original_k = k;
+
+    // Test all possible combinations of formations
+    for (int strategy = 1; strategy <= 3; strategy++) {
+        long long int count = 0;
+        n = original_n;
+        m = original_m;
+        k = original_k;
+
+        // Strategy 1: Prioritize Formation 3, then Formation 1
+        if (strategy == 1) {
+            // Formation 3 first
+            while (n >= 1 && m >= 1 && k >= 1) {
+                count++;
+                n--;
+                m--;
+                k--;
+            }
+            // Then Formation 1
+            while (n >= 2 && k >= 1) {
+                count++;
+                n -= 2;
+                k--;
+            }
+        }
+        // Strategy 2: Prioritize Formation 2, then Formation 1
+        else if (strategy == 2) {
+            // Formation 2 first
+            while (n >= 2 && m >= 1 && k >= 1) {
+                count++;
+                n -= 2;
+                m--;
+                k--;
+            }
+            // Then Formation 1
+            while (n >= 2 && k >= 1) {
+                count++;
+                n -= 2;
+                k--;
+            }
+        }
+        // Strategy 3: Formation 1 only
+        else {
+            while (n >= 2 && k >= 1) {
+                count++;
+                n -= 2;
+                k--;
+            }
+        }
+
+        if (count > max_count) {
+            max_count = count;
         }
     }
 
-    else if (n > 0 && m > 0 && k > 0)
-    {
-        // printf("hello from 3rd form");
-
-        while (n >= 1 && k >= 1 && m >= 1)
-        {
-            katryoshka_count++; // katryoshka count will increase according to the first formation
-            n -= 1;             // eyes count will decrease by 1 because 1 eyes will be used to form a katryoshka
-            k--;                // 1 body count will decrase because one body will be taken for forming a katryoshka
-        }
-    }
-
-    printf("%lld", katryoshka_count);
+    printf("%lld", max_count);
     return 0;
 }
