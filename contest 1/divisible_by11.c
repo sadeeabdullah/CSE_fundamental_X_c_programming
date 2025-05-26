67494769cde6881a3722cfda619db8a72bd2d557
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-05-26 08:00:44 +0600

    added almost palindrome string solution

--- contest 1/divisible_by11.c
+++ contest 1/divisible_by11.c
@@ -1,31 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include<math.h>
 int main(){
-    long long int n;
-    int sum_of_even_digits = 0;
-    int sum_of_odd_digit = 0;
-    int digit;
-    scanf("%lld", &n);
-    //for loop for getting the odd and even position digit sum
-    for (int i = 1; n > 0; i++)
-    {
-        digit = n % 10;
-        if (i % 2 == 0)
-        {
-            sum_of_even_digits += digit ;
-        }else{
+    char a[102];
+    scanf("%s", &a);
 
-            sum_of_odd_digit += digit;
-        }
-        n /= 10;
-    }
-
-    int difference = sum_of_odd_digit - sum_of_even_digits;
-    if (difference % 11 == 0)
+    //taking variables to store the summation of even and odd digits number
+    int sum_of_even_numbers = 0, sum_of_odd_numbers = 0;
+    int l_n_string = strlen(a);
+    for (int i = 0; i < l_n_string; i++)
+{
+    if ((i + 1)   % 2 == 0) // even position (2nd, 4th, ...)
     {
-        printf("YES");
-    }else{
-        printf("NO");
+        // printf("the digit is even number %d digit position%d\n", i, (i + 1));
+        sum_of_even_numbers += a[i] - '0';
+    } else { 
+        // printf("the digit is odd number %d digit position%d\n", i, (i + 1));
+        sum_of_odd_numbers += a[i] - '0';
     }
+}
+
+    // getting absolute difference between even and odd summation
+    int abs_difference = abs(sum_of_odd_numbers - sum_of_even_numbers);
+    (abs_difference % 11 == 0)?printf("YES"):printf("NO");
     
     
     return 0;
